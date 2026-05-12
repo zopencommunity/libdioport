9b657177d1af7251f2e06185648d2db66d771071
Sachin <32639496+sachintu47@users.noreply.github.com>
2026-05-12 18:46:48 +0530

    Update Git checkout URL in cicd-dev.groovy
    Signed-off-by: Sachin <32639496+sachintu47@users.noreply.github.com>

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -7,6 +7,13 @@ node('linux')
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
       userRemoteConfigs: [[url: 'https://github.com/zopencommunity/libdioport.git']]])
+// Poll changes from upstream
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: "https://github.com/zopencommunity/libdio.git"]]])
   }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libdioport.git'), string(name: 'PORT_DESCRIPTION', value: 'Dataset I/O' ), string(name: 'BUILD_LINE', value: 'DEV') ]
